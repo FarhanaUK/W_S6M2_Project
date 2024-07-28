ef2c7fbace09640de2910d760c27b27c0673156c
Farhana Aktar <farhanaaktar@live.co.uk>
2024-07-28 17:47:46 +0100

    complete

--- frontend/components/Posts/Posts.js
+++ frontend/components/Posts/Posts.js
@@ -5,8 +5,14 @@ const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
+ 
+
   return (
     <div className='posts-container-wrapper'>
+    { 
+    posts.map(element => <Post likePost ={likePost} post={element} key={element.id}/>)
+    }
+ 
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
